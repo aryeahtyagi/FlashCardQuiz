e5ad1f9819de7a637d8a82f4c706a9b4d0c6889e
arya tyagi <aryeahtyagi@gmail.com>
2022-03-05 14:52:42 +0530

    Design compeleted for questions fragment

---
+++ app/src/main/java/com/atria/software/flashcardquiz/ui/blockbuster/customviews/Utils.kt
@@ -0,0 +1,20 @@
+package com.atria.software.flashcardquiz.ui.blockbuster.customviews
+
+
+import android.content.Context
+import android.util.TypedValue
+
+/**
+ * Created by SamuelGjk on 2019/6/13.
+ */
+object Utils {
+    @JvmStatic
+    fun dp2px(context: Context, dp: Float): Float {
+        return TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            dp,
+            context.resources.displayMetrics
+        )
+
+    }
+}
